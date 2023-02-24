0b6287cf485c212a32cc456876ae86a2f11210aa
Saverio Guzzo <saveriog@spotify.com>
2023-02-24 14:05:48 +0100

    Use different directory

--- slskd/start.sh
+++ slskd/start.sh
@@ -2,4 +2,4 @@ curl -X PATCH --header "Content-Type:application/json" \
     --data '{"network": {"hostname": "slskd"}}' \
     "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
 
-exec slskd serve --iknowwhatimdoing --host 0.0.0.0 --basedir "/app"
+exec slskd serve --iknowwhatimdoing --host 0.0.0.0 --basedir "/slskd"
